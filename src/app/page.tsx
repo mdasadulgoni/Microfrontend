bddeed3f049f805036a3adcdfa75641a0d38d973
mdasadulgoni <mdasadulgoni@gmail.com>
2023-12-02 08:16:22 +0530

    commit .eslint add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import React from "react";
+// 1. Import Area 
 
-export default function Home() {
+const name = 'Md Asadul Goni'; // This is a global variable
+// 2. Function Define Area 
+function Home() {
+  //2.1 Hooks Area
+
+  // 2.2 Function Define Area
+  const company = 'ovoserve.com'; // This is a global variable 
+
+  // 2.3 Return Somthing 
   return (
     <>
-      <h1>Testing</h1>    
+      <h1>Testing {name}{company}</h1>    
     </>
   )
 }
+// 3. Export Area 
+// 3.1 Default Export
+export default Home;
+// 3.2 Named Export Area 
