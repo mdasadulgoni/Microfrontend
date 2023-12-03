06113689f51a421b285d662e9decc01f16d0144a
mdasadulgoni <mdasadulgoni@gmail.com>
2023-12-03 19:08:23 +0530

    precommit test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
-import React from "react";
-// 1. Import Area 
+import React from 'react'
+// 1. Import Area
 
-const name = 'Md Asadul Goni'; // This is a global variable
-// 2. Function Define Area 
+ const name = 'Md Asadul Goni' // This is a global variable
+// 2. Function Define Area
 function Home() {
   //2.1 Hooks Area
 
   // 2.2 Function Define Area
-  const company = 'ovoserve.com'; // This is a global variable 
+   const company = 'ovoserve.com' // This is a global variable
 
-  // 2.3 Return Somthing 
+  // 2.3 Return Somthing
   return (
     <>
-      <h1>Testing {name}{company}</h1>    
-    </>
+                            <h1>
+        Testing {name} {company}
+                                         </h1>
+                  </>
   )
 }
-// 3. Export Area 
+// 3. Export Area
 // 3.1 Default Export
-export default Home;
-// 3.2 Named Export Area 
+export default Home
+// 3.2 Named Export Area
